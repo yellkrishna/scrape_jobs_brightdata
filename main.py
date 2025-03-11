2fd890ff48cce568e38500ac1b7c1c9b21b35890
yellkrishna <69414784+yellkrishna@users.noreply.github.com>
2025-03-11 13:17:59 -0400

    cosmtic change

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import pandas as pd
+import os
 
 from crawler import dfs_crawl
 
 if __name__ == '__main__':
     # Set up server address and target URL.
-    server_addr = 'https://brd-customer-hl_cf240944-zone-scrape_job_postings:si9klwfh7f28@brd.superproxy.io:9515'
+    server_addr = os.getenv('CHROME_SERVER_ADDR')
     TARGET_URL = 'https://fieracapital.wd3.myworkdayjobs.com/Career'
     
     all_job_links, all_job_postings = dfs_crawl(server_addr, TARGET_URL, max_depth=3, max_breadth=3)
